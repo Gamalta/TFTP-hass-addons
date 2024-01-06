5976256ba1be21aef608618bfa86a64a5370986b
Gamalta <eliovital@icloud.com>
2024-01-06 19:51:15 +0100

    chore: rename run.sh to start.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-
-echo "Starting server."
-echo "Sharing files:"
-ls /tftpboot
-
-in.tftpd -L -vvv -u ftp --secure /tftpboot
---
+++ start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+echo ""
+echo "----------------------------------------"
+echo " Starting tftpd,                        "
+echo "                                        "
+echo "Starting server."
+echo "Sharing files:"
+tree /srv/tftp
+echo "                                        "
+echo "                                        "
+echo "----------------------------------------"
+echo ""
+
+in.tftpd -u tftpd --foreground --address 0.0.0.0:69 --secure /srv/tftp
