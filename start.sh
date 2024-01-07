89ddeb8a17c7d939f720890b184cadf1b91bef22
Gamalta <eliovital@icloud.com>
2024-01-07 04:37:34 +0100

    fix: config path

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/with-contenv bashio
 set -e
-bashio::log.info ""
 bashio::log.info "----------------------------------------"
-bashio::log.info " Starting Addon: $(bashio::addon.name)  "
+bashio::log.info "Starting Addon: $(bashio::addon.name)   "
 bashio::log.info "                                        "
 bashio::log.info "Starting TFTP server."
 in.tftpd -u tftpd --foreground --address 0.0.0.0:69 --secure /srv/tftp &
@@ -11,14 +10,13 @@ ls /srv/tftp
 bashio::log.info "                                        "
 bashio::log.info "                                        "
 bashio::log.info "----------------------------------------"
-bashio::log.info ""
 
 # Read from STDIN
 while read -r input; do
     input=$(bashio::jq "${input}" '.')
     bashio::log.info "Read alias: $input"
 
-    for config in $(bashio::config 'config|keys'); do
+    for config in $(bashio::config 'configs|keys'); do
         ID=$(bashio::config "configs[${config}].id")
         MESSAGE=$(bashio::config "configs[${config}].message")
   
